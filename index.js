f0516ce7f081dd5ef44dfc063a276ee351020fe5
romero23082000 <andreshito435@gmail.com>
2023-02-05 22:53:47 -0500

    cors vercel

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
+var morgan = require('morgan')
+morgan(':method :url :status :res[content-length] - :response-time ms')
+
+app.use(express.json())
+app.use(morgan('combined'))
+app.use(cors())
+
 
 let notes = [
   {
@@ -22,6 +30,39 @@ let notes = [
   }
 ]
 
+let persons = [
+  {
+    "name": "Dan Abramov",
+    "number": "12-43-234345",
+    "id": 3
+  },
+  {
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122",
+    "id": 4
+  },
+  {
+    "name": "Andres ",
+    "number": "3234342323",
+    "id": 5
+  }
+]
+
+
+const generateId = () => {
+  const maxId = notes.length > 0
+    ? Math.max(...notes.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
+const generateIdPeople = () => {
+  const maxId = persons.length > 0
+    ? Math.max(...persons.map(n => n.id))
+    : 0
+  return maxId + 1
+}
+
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
@@ -30,10 +71,89 @@ app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
+})
+
+app.get('/api/info', (request, response) => {
+  const countPersons = persons.length
+  const date = new Date()
+  response.send(`<p>Phonebook has info for ${countPersons} people</p><p>${date}</p>`)
+})
+
 app.get('/api/notes/:id', (request, response) => {
   const id = Number(request.params.id)
   const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(person => person.id === id)
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(people => people.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const body = request.body
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: 'content missing'
+    })
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  }
+
+  notes = notes.concat(note)
+
   response.json(note)
+})
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing'
+    })
+  }
+
+  const people = {
+    id: generateIdPeople(),
+    name: body.name,
+    number: body.number,
+  }
+
+  persons = persons.concat(people)
+
+  response.json(people)
 })
 
 const PORT = 3001
