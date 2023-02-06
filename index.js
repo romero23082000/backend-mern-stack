1cd24b36cb89fec5f7d6ad046c9c09b248c5860a
romero23082000 <andreshito435@gmail.com>
2023-02-06 15:01:46 -0500

    Model notes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
-var morgan = require('morgan')
-morgan(':method :url :status :res[content-length] - :response-time ms')
+const mongoose = require('mongoose')
+const Note = require('./models/note')
 
 app.use(express.json())
-app.use(morgan('combined'))
 app.use(cors())
 
 
@@ -30,35 +30,9 @@ let notes = [
   }
 ]
 
-let persons = [
-  {
-    "name": "Dan Abramov",
-    "number": "12-43-234345",
-    "id": 3
-  },
-  {
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122",
-    "id": 4
-  },
-  {
-    "name": "Andres ",
-    "number": "3234342323",
-    "id": 5
-  }
-]
-
-
 const generateId = () => {
   const maxId = notes.length > 0
     ? Math.max(...notes.map(n => n.id))
-    : 0
-  return maxId + 1
-}
-
-const generateIdPeople = () => {
-  const maxId = persons.length > 0
-    ? Math.max(...persons.map(n => n.id))
     : 0
   return maxId + 1
 }
@@ -68,51 +42,47 @@ app.get('/', (request, response) => {
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
-})
-
-app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})
-
-app.get('/api/info', (request, response) => {
-  const countPersons = persons.length
-  const date = new Date()
-  response.send(`<p>Phonebook has info for ${countPersons} people</p><p>${date}</p>`)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
 app.get('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const note = notes.find(note => note.id === id)
-  if (note) {
-    response.json(note)
-  } else {
-    response.status(404).end()
-  }
-})
-
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => {
+      console.log(error)
+      response.status(400).send({ error: 'malformatted id' })
+    })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Note.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(people => people.id !== id)
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
-  response.status(204).end()
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/notes', (request, response) => {
@@ -124,37 +94,17 @@ app.post('/api/notes', (request, response) => {
     })
   }
 
-  const note = {
+  const note = new Note({
     content: body.content,
     important: body.important || false,
     date: new Date(),
-    id: generateId(),
-  }
+  })
 
-  notes = notes.concat(note)
-
-  response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
-app.post('/api/persons', (request, response) => {
-  const body = request.body
-
-  if (!body.number) {
-    return response.status(400).json({
-      error: 'number missing'
-    })
-  }
-
-  const people = {
-    id: generateIdPeople(),
-    name: body.name,
-    number: body.number,
-  }
-
-  persons = persons.concat(people)
-
-  response.json(people)
-})
 
 const PORT = 3001
 app.listen(PORT, () => {
---
+++ mongo.js
@@ -0,0 +1,22 @@
+
+
+
+
+
+// const note = new Note({
+//   content: 'HTML is Easy',
+//   date: new Date(),
+//   important: true,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+Note.find({}).then(result => {
+  result.forEach(note => {
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
