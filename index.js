73c1ced0088779162bfdc7e1cea9333517a8ea7e
romero23082000 <andreshito435@gmail.com>
2023-02-05 21:02:29 -0500

    add versel.json

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
 
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  response.json(note)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
